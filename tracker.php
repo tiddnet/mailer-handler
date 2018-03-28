306edb6b56086002bf8111bfa749c539dfdf8e7e
Todd C. Campbell <tiddnet@users.noreply.github.com>
2018-03-28 18:16:24 -0400

    Update tracker.php

--- tracker.php
+++ tracker.php
@@ -1,14 +1,6 @@
 <?php
 include_once('my_func.php');
-// Get input, record it. 
 
-//$my_id; 
-//$my_campaign;
-// $track_ip = getUserIP();
-
-
-//echo "<br>UA: $track_ua";
-//echo "<br>IP: $track_ip";
 $result = emailOpen($conn, $my_email, $my_campaign, $track_ua);
 
 
